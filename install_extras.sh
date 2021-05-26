8c05fc6d577c8ba7a854ae843f9adedadb1d2a98
Eraldo Barbosa dos Anjos Filho <ebaf2@cin.ufpe.br>
2021-05-26 10:44:48 -0300

    Create install_extras.sh

---
+++ install_extras.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## build ARGs
+NCPUS=${NCPUS:-1}
+
+set -e
+apt-get update -qq && apt-get -y --no-install-recommends install \
+    libxml2-dev \
+    libcairo2-dev \
+    libgit2-dev \
+    default-libmysqlclient-dev \
+    libpq-dev \
+    libsasl2-dev \
+    libsqlite3-dev \
+    libssh2-1-dev \
+    libxtst6 \
+    libcurl4-openssl-dev \
+    unixodbc-dev && \
+  rm -rf /var/lib/apt/lists/*
+
+## shiny
+install2.r --error --skipinstalled -r $CRAN -n $NCPUS \
+    shiny \
+    shinydashboard \
+    DT \
+		plotly \
+    leaflet 
+
+## database
+install2.r --error --skipinstalled -r $CRAN -n $NCPUS \
+    pool \
+    odbc
+
+## extras
+install2.r --error --skipinstalled -r $CRAN -n $NCPUS \
+    janitor \
+    gtsummary
+
+rm -rf /tmp/downloaded_packages
